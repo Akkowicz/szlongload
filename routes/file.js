7d115fe204f69d5e343d4ed1c41f144c39fe357d
Mateusz Przybyłowicz <uamfhq@gmail.com>
2018-12-14 19:33:51 +0100

    Change to memory storage

--- routes/file.js
+++ routes/file.js
@@ -6,14 +6,7 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads');
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}${path.extname(file.originalname)}`);
-    }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
