568b80f69fc74e0981008064ddcb2d88ee679c23
Mateusz Przybyłowicz <uamfhq@gmail.com>
2018-12-16 21:43:07 +0100

    Add client-side caching

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join('./', 'public')));
+app.use(express.static(path.join('./', 'public'), { maxAge: 600000 }));
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
